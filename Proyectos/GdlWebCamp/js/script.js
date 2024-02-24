74bf663777374ecf5cc64d523a328e2dfdf27e9b
rafael07-dev <deralato817@gmail.com>
2024-02-24 16:02:17 -0500

    add transitions

--- Proyectos/GdlWebCamp/js/script.js
+++ Proyectos/GdlWebCamp/js/script.js
@@ -14,3 +14,35 @@ btnClose.addEventListener('click', ()=>{
     }
     return;
 });
+
+
+const cards = document.querySelectorAll('.card-precios');
+const numero = document.querySelectorAll('.numero');
+
+window.addEventListener('scroll', animationScroll);
+
+animationScroll();
+
+function animationScroll() {
+    const windowHeight = window.innerHeight / 5 * 4;
+
+    cards.forEach((card) => {
+        const heightCard = card.getBoundingClientRect().top;
+
+        if (heightCard < windowHeight) {
+            card.classList.toggle('show');
+        }else{
+            card.classList.remove('show');
+        }
+    });
+
+    numero.forEach((numero) => {
+        const heightCard = numero.getBoundingClientRect().top;
+
+        if (heightCard < windowHeight) {
+            numero.classList.toggle('show');
+        }else{
+            numero.classList.remove('show');
+        }
+    });
+}
